adddcee757d42664748ebbc395d580f0816ad114
mauz <maurizio.donato@gmail.com>
2014-08-19 18:49:29 +0200

    ping pong

--- DigitalArx/src/com/digitalarx/android/utils/CipherUtil.java
+++ DigitalArx/src/com/digitalarx/android/utils/CipherUtil.java
@@ -5,35 +5,35 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class CipherUtil {
-	
-	
+
+
 	private static SecretKey skey = null;
-	
+
 	public CipherUtil(String password) {
 		skey = new SecretKeySpec(password.getBytes(), "AES");
 	}
 
 	public static byte[] encodeFile(byte[] fileData) throws Exception
-    {
+	{
 
-        SecretKeySpec skeySpec = new SecretKeySpec(skey.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+		SecretKeySpec skeySpec = new SecretKeySpec(skey.getEncoded(), "AES");
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
 
-        byte[] encrypted = cipher.doFinal(fileData);
+		byte[] encrypted = cipher.doFinal(fileData);
 
-        return encrypted;
-    }
+		return encrypted;
+	}
 
-    public static byte[] decodeFile(byte[] fileData) throws Exception
-    {
-        SecretKeySpec skeySpec = new SecretKeySpec(skey.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+	public static byte[] decodeFile(byte[] fileData) throws Exception
+	{
+		SecretKeySpec skeySpec = new SecretKeySpec(skey.getEncoded(), "AES");
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.DECRYPT_MODE, skeySpec);
 
-        byte[] decrypted = cipher.doFinal(fileData);
+		byte[] decrypted = cipher.doFinal(fileData);
 
-        return decrypted;
-    }
+		return decrypted;
+	}
 
 }
