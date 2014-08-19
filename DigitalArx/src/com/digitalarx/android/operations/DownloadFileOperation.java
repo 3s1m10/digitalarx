d2e855a3abea9a98cc2bfebe1b64c8f386c0d179
mauz <maurizio.donato@gmail.com>
2014-08-19 20:40:35 +0200

    prova 123

--- DigitalArx/src/com/digitalarx/android/operations/DownloadFileOperation.java
+++ DigitalArx/src/com/digitalarx/android/operations/DownloadFileOperation.java
@@ -147,7 +147,7 @@ public class DownloadFileOperation extends RemoteOperation {
         
         if (result.isSuccess()) {
             
-        	//giorgiojalamoff
+        
         	
         	mModificationTimestamp = mDownloadOperation.getModificationTimestamp();
             newFile = new File(getSavePath());
